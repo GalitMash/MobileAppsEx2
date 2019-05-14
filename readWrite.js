7dba0f6592e25f7892a7222594983ddd544dc77d
GalitMash <galitmash@gmail.com>
2019-05-14 11:16:07 +0300

    Cleaned readWrite a bit

--- readWrite.js
+++ readWrite.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 let reverseData = "";
 
-fs.readFile('/a.txt', (err, data) => {
-    if (err !== null) {
-        console.log("An error has occurred while reading the file: " + err.name + ": " + err.message);
-        return;
-    }
-    reverseData = reverseString(data);
-});
-
-fs.writeFile('/b.txt', reverseData, (err) => {
-    if (err) {
-        console.log("An error has occurred while writing the file: " + err.name + ": " + err.message);
-    }
-});
+try {
+    fs.readFile('/a.txt', (err, data) => {
+        if (err) throw err;
+        reverseData = reverseString(data);
+    });
+    fs.writeFile('/b.txt', reverseData, (err) => {
+        if (err) throw err;
+    });
+} catch (err) {
+    console.log("An error has occurred while reading/writing: " + err.name + ": " + err.message);
+}
 
 function reverseString(str) {
     return str.split("").reverse().join("");
